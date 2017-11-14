08f43a06c1181a123a75aef2ded57da36339e45e
Евгения Масликова < >
2017-11-14 19:34:51 +0300

    final version

--- richhouse/js/popUp.js
+++
@@ -1,1 +0,0 @@
-function PopUpShow(){$("#popup1").show(),$("#blindLayer").css("display","block")}function PopUpHide(){$("#popup1").hide(),$("#blindLayer").css("display","none")}$(document).ready(function(){PopUpHide()});
